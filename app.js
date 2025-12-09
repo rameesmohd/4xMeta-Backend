e10374a3041655c3b96dad16b5a07a3c6c782cdd
rameesmohd <rameesmohd789@gmail.com>
2025-12-10 01:55:27 +0530

    modified:   app.js 	modified:   services/rolloverService.js

--- app.js
+++ app.js
@@ -64,9 +64,9 @@ app.use(cors(corsOptions));
 app.use(express.json({ limit: "10mb" })); 
 app.use(express.urlencoded({ limit: "10mb", extended: true })); 
 app.use(cookieParser());
-require('./services/rolloverService.js')
-require('./services/intervalservice.js')
-require('./services/broadcastWorker.js')
+// require('./services/rolloverService.js')
+// require('./services/intervalservice.js')
+// require('./services/broadcastWorker.js')
 
 
 const limiter = rateLimit({
